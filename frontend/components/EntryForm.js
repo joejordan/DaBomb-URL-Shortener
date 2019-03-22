5cccec5dbebc62b71693f44971400dda6c5f66b6
Joe Jordan <github@burgtech.com>
2019-03-22 12:58:42 -0400

    React Hooks: wow setting state it a lot simpler

--- frontend/components/EntryForm.js
+++ frontend/components/EntryForm.js
@@ -2,12 +2,12 @@ import { useState } from 'react';
 
 function EntryForm() {
   // useState returns a pair: the current state value and a function to update it
-  const [urlToAdd, setUrl] = useState('http://google.com');
+  const [urlToAdd, setUrl] = useState('');
 
   return (
     <>
       <form>
-        <input type="text" name="url" />
+        <input type="text" name="url" onChange={e => setUrl(e.target.value)} />
         <input type="submit" value="Shorten URL" />
       </form>
       <div>Url that was input: {urlToAdd}</div>
